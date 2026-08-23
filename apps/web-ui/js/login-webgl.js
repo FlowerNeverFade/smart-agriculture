6838a8e7dcb9a093e566403ce2c3d4ba49c49887
FlowerNeverFade <1106459072@qq.com>
2026-08-23 23:37:09 +0800

    style(ui): set idle water-ink flow to 36px per second

--- apps/web-ui/js/login-webgl.js
+++ apps/web-ui/js/login-webgl.js
@@ -270,7 +270,7 @@ const DYE_FRAGMENT_SHADER = `#version 300 es
         - 0.45 * 1.08 * cos(phaseB)
         - 0.18 * 1.76 * cos(phaseC)
     );
-    return field * (26.0 / max(uCssMinDimension, 1.0)) / 1.073;
+    return field * (36.0 / max(uCssMinDimension, 1.0)) / 1.073;
   }
 
   void main() {
